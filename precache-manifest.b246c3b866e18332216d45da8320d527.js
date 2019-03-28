91959743959bd5e2cb6873b59fb3eaa2a61befa3
Septiya Tamara <septiyakriistin98@mail.ugm.ac.id>
2019-03-28 08:46:08 +0700

    Updates

---
+++ precache-manifest.b246c3b866e18332216d45da8320d527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/WEBFirstHome/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b88f59272303e7745f8a",
+    "url": "/WEBFirstHome/static/js/runtime~main.f6a985ec.js"
+  },
+  {
+    "revision": "ebd5aa65ee3f82172bb9",
+    "url": "/WEBFirstHome/static/js/main.81f44b15.chunk.js"
+  },
+  {
+    "revision": "169d65e6dbdd65ef96e0",
+    "url": "/WEBFirstHome/static/js/2.d7d3ebc6.chunk.js"
+  },
+  {
+    "revision": "ebd5aa65ee3f82172bb9",
+    "url": "/WEBFirstHome/static/css/main.9c3c4a8b.chunk.css"
+  },
+  {
+    "revision": "3b3fa8a76b0390de22c30d3912792b98",
+    "url": "/WEBFirstHome/index.html"
+  }
+];
