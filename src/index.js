e58f156061beeb385d814cd2a53cf78b588b61a5
Septiya Tamara <septiyakriistin98@mail.ugm.ac.id>
2019-03-28 08:02:08 +0700

    nyoba

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Custom.css'; //import cssnya
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { appendFile } from 'fs';
+
+//const items = ['Merah','Kuning','Hijau','Biru'];
+
+const items = [
+  {
+    name : "Summer",
+    color : 'Merah',
+    img :'Panas.jpg'
+  },
+  {
+    name : "Auntum",
+    color : 'Kuning',
+    img :'Gugur.jpg'
+  },
+  {
+    name : "Spring",
+    color : 'Hijau',
+    img :'Semi.jpg'
+  }, {
+    name : "Winter",
+    color : 'Biru',
+    img :'Salju.jpg'
+  },
+];
+
+ReactDOM.render(<App items={items}/>,
+  document.getElementById('root')
+);
+serviceWorker.register();
